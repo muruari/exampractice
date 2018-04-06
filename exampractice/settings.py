34cf2ff04eaf20fc78a6be98affc9497241d7262
Robert Murua <muruari@gmail.com>
2018-04-06 15:24:59 -0700

    updating reg validations

--- exampractice/settings.py
+++ exampractice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gcn(zqu@ybw(4x=ysrq!9%e)4ne@2zxhm1(^*p2lx!e(6$er5i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.183.137.228']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
